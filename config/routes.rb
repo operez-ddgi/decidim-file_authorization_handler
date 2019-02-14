dfeaf0d35b73e5523af8bd129196f7110cdc2859
Oscar Perez <operez@ddgi.cat>
2019-02-14 15:51:20 +0100

    Replaced single quotes by double quotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Decidim::FileAuthorizationHandler::AdminEngine.routes.draw do
-  post '/delete_authorizations' => 'censuses#delete_authorizations'
+  post "/delete_authorizations" => "censuses#delete_authorizations"
 end
